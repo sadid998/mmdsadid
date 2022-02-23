ff59f405316a398b9f154fc7e07a78c76701cbca
sadid998 <75202585+sadid998@users.noreply.github.com>
2022-02-23 22:46:40 +0330

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -56,7 +56,9 @@ async def download(event):
             id_hex = hex(msg.id)[2:]
             id = f"{id_hex}/{get_file_name(msg)}"
             bot_url = f"t.me/{username_bot}?start={id_hex}"
-            await event.reply(f"Link to download file: \n\n📎 : {Config.DOMAIN}/{id}\n\n🤖 : {bot_url}")
+            response = requests.get(f"https://droplink.co/api?api=0b8645cc45284e92d722e59c60d918291526a16c&url={Config.DOMAIN}/{id}").text
+            linkk = (json.loads(response)["shortenedUrl"])
+            await event.reply(f"Link to download file: \n\n📎 : {linkk}\n\n🤖 : {bot_url}")
             return
 
         elif id_msg := re.search("/start (.*)", event.raw_text ):
